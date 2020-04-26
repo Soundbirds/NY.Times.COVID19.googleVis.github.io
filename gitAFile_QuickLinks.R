d3b46c7fc0d45414282f7ec1ffb9b07ca1466016
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-04-26 13:25:28 -0700

    Update gitAFile_QuickLinks.R

--- gitAFile_QuickLinks.R
+++ gitAFile_QuickLinks.R
@@ -5,3 +5,5 @@ JRWToolBox::gitAFile() quick links:
 
      gitEdit('gVisCOVID.NYT.Data.R', "Soundbirds/NY.Times.COVID19.googleVis.github.io/master/")
 
+     gitEdit('updateGvisFigs.R', "Soundbirds/NY.Times.COVID19.googleVis.github.io/master/")
+
