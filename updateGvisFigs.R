e115ca1220b96b9793aca18b293d07a5fad47131
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-04-26 17:07:15 -0700

    Delete updateGvisFigs.R

--- updateGvisFigs.R
+++
@@ -1,8 +0,0 @@
-
-# Create googleVis html files for publishing on the GitHub website
-
-gVisCOVID.NYT.Data(Print = TRUE)  # States
-
-gVisCOVID.NYT.Data(c('Washington', 'New York', 'Michigan'), Print = TRUE)  # Counties within the listed states
-
-
