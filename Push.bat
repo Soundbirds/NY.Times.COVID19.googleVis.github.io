92e75bc9eb8411eaad02529b5cd7fc9d595eaca0
Soundbirds <61962365+Soundbirds@users.noreply.github.com>
2020-05-28 01:33:25 -0700

    Public

--- Push.bat
+++ Push.bat
@@ -1,4 +1,4 @@
-cd  C:\Users\John\NY.Times.COVID19.googleVis.github.io
+cd  C:\Users\Public\NY.Times.COVID19.googleVis.github.io
 git add COVID_counties.htm
 git add COVID_states.htm
 git add index.htm
